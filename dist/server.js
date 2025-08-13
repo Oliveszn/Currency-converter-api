8479cf06fd08e314e55a8f03e45c77bfca4b8500
oliveszn <ekpealamicheal@gmail.com>
2025-08-13 10:34:05 +0100

    untracked dist

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-console.log("Hello from ts");
-//# sourceMappingURL=server.js.map
